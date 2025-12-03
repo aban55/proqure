f558b9512ce5aef9f4bf9472bb3cd3b0e2ba6bd6
Abhishek Bansal <abhishekbansal@Abhisheks-MacBook-Air.local>
2025-12-03 08:19:59 +0530

    fix(zustand): use named import in userStore copy

---
+++ store/userStore copy.ts
@@ -0,0 +1,19 @@
+// store/userStore.ts
+import { create } from "zustand";
+
+type User = {
+  id?: string;
+  name?: string;
+  email?: string;
+};
+
+type State = {
+  user?: User;
+  setUser: (u?: User) => void;
+};
+
+export const useUserStore = create<State>((set) => ({
+  user: undefined,
+  setUser: (u) => set({ user: u }),
+}));
+
